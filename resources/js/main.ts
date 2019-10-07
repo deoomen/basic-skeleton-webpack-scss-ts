cfe17f6ac05f10e2d36c5489e9a0e1d5c5dd0b29
deoomen <amadeusz.walkiewicz92@gmail.com>
2019-10-07 21:46:55 +0200

    ts  pages

---
+++ resources/js/main.ts
@@ -0,0 +1,13 @@
+// global styles
+import '../scss/main';
+
+// global scripts
+// eg.: import 'bootstrap' or import './myscripts'
+
+export default class Main {
+
+    constructor() {
+
+    }
+
+}
---
+++ resources/js/pages/home.ts
@@ -0,0 +1,15 @@
+// styles
+import '../../scss/home';
+
+// scripts
+import Main from '../main';
+
+class Home extends Main {
+
+    constructor() {
+        super();
+    }
+
+}
+
+const home = new Home();
